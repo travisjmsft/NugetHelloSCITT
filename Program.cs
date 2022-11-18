21a7af137537e838204f0a6f198b56d19c1764c6
travisjmsft <108142766+travisjmsft@users.noreply.github.com>
2022-11-17 20:18:19 -0800

    added stuff

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, SCITT World! These are some of my favorite things:");
+        List<string> favoriteThings = new List<string>
+        {
+            "Pizza",
+            "Beer",
+            "Baseball"
+        };
+      
+      string json = JsonConvert.SerializeObject(favoriteThings);
+      Console.WriteLine(json);
+    }
+}
